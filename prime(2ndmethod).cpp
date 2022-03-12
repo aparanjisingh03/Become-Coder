b81135da7fb6659f0ac2b5544be2663af39650db
Aparanji Singh <84464895+aparanjisingh03@users.noreply.github.com>
2022-03-12 12:11:14 +0530

    Add files via upload

---
+++ prime(2ndmethod).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i,count=0;
+	cin>>n;
+	for(i=2;i<n;i++)
+	{
+		if(n%i==0)
+		{
+			count++;
+		}
+	}
+	if(count==0)
+	cout<<"prime";
+	else
+	cout<<"not prime";
+}
